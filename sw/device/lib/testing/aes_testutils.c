lowRISC/opentitan

[test] Ignore CSRNG alert during KAT test
The KAT test generates the all-zero genbits twice in a row (before and
after reseeding). CSRNG has a recoverable alert that detects when the
same genbits were generated twice as it indicates an attack.

We have to disable this alert for the second generation and restore it
afterwards.

Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>
(cherry picked from commit 364b6e88843e405dab526dafc030a586c75d8cf0)

sw/device/lib/testing/aes_testutils.c
@@ -6,6 +6,7 @@
 
 #include "hw/ip/aes/model/aes_modes.h"
 #include "sw/device/lib/dif/dif_aes.h"
+#include "sw/device/lib/dif/dif_alert_handler.h"
 #include "sw/device/lib/testing/test_framework/check.h"
 
 #ifndef OPENTITAN_IS_ENGLISHBREAKFAST
@@ -196,11 +197,43 @@ status_t aes_testutils_csrng_kat(const dif_csrng_t *csrng) {
   TRY(csrng_testutils_kat_reseed(csrng, &seed_material_reseed,
                                  &expected_state_reseed));
 
+  // We're about to generate the same `genbits` again which will trigger a
+  // `csrng_recov_alert`. This is expected, so we must disable this alert if it
+  // is currently enabled and restore it after.
+  dif_alert_handler_t alert_handler;
+  CHECK_DIF_OK(dif_alert_handler_init(
+      mmio_region_from_addr(TOP_EARLGREY_ALERT_HANDLER_BASE_ADDR),
+      &alert_handler));
+
+  dif_toggle_t was_enabled = kDifToggleDisabled;
+  CHECK_DIF_OK(dif_alert_handler_alert_is_enabled(
+      &alert_handler, kTopEarlgreyAlertIdCsrngRecovAlert, &was_enabled));
+
+  if (was_enabled == kDifToggleEnabled) {
+    CHECK_DIF_OK(dif_alert_handler_alert_set_enabled(
+        &alert_handler, kTopEarlgreyAlertIdCsrngRecovAlert,
+        kDifToggleDisabled));
+  }
+
   // Generate one block containing the required seed for the AES masking PRNG
   // to output an all-zero vector.
   TRY(csrng_testutils_kat_generate(csrng, 1, kCsrngBlockLen, NULL,
                                    kAesMaskingPrngZeroOutputSeed,
                                    &expected_state_generate));
+
+  // Check that the correct recoverable alert fired even if the alert was
+  // ignored.
+  uint32_t recov_alerts = 0;
+  CHECK_DIF_OK(dif_csrng_get_recoverable_alerts(csrng, &recov_alerts));
+  CHECK(recov_alerts == kDifCsrngRecoverableAlertRepeatedGenBits);
+  CHECK_DIF_OK(dif_csrng_clear_recoverable_alerts(csrng));
+
+  // Restore the alert if it was enabled.
+  if (was_enabled == kDifToggleEnabled) {
+    CHECK_DIF_OK(dif_alert_handler_alert_set_enabled(
+        &alert_handler, kTopEarlgreyAlertIdCsrngRecovAlert, kDifToggleEnabled));
+  }
+
   return OK_STATUS();
 }
 #endif

apinise/pavona

sw/device/lib/testing/aes_testutils.c
@@ -5,10 +5,13 @@
 #include "sw/device/lib/testing/aes_testutils.h"
 
 #include "hw/ip/aes/model/aes_modes.h"
+#include "hw/top/dt/alert_handler.h"
 #include "sw/device/lib/dif/dif_aes.h"
+#include "sw/device/lib/dif/dif_alert_handler.h"
 #include "sw/device/lib/testing/test_framework/check.h"
 
 #ifdef AES_TESTUTILS_HAS_EDN_AND_CSRNG
+#include "hw/top/dt/csrng.h"
 #include "sw/device/lib/dif/dif_csrng_shared.h"
 #include "sw/device/lib/testing/csrng_testutils.h"
 
@@ -196,11 +199,43 @@ status_t aes_testutils_csrng_kat(const dif_csrng_t *csrng) {
   TRY(csrng_testutils_kat_reseed(csrng, &seed_material_reseed,
                                  &expected_state_reseed));
 
+  // We're about to generate the same `genbits` again which will trigger a
+  // `csrng_recov_alert`. This is expected, so we must disable this alert if it
+  // is currently enabled and restore it after.
+  dif_alert_handler_t alert_handler;
+  TRY(dif_alert_handler_init_from_dt(kDtAlertHandler, &alert_handler));
+
+  dt_alert_id_t csrng_recov_alert =
+      dt_csrng_alert_to_alert_id(kDtCsrng, kDtCsrngAlertRecovAlert);
+
+  dif_toggle_t was_enabled = kDifToggleDisabled;
+  TRY(dif_alert_handler_alert_is_enabled(&alert_handler, csrng_recov_alert,
+                                         &was_enabled));
+
+  if (was_enabled == kDifToggleEnabled) {
+    TRY(dif_alert_handler_alert_set_enabled(&alert_handler, csrng_recov_alert,
+                                            kDifToggleDisabled));
+  }
+
   // Generate one block containing the required seed for the AES masking PRNG
   // to output an all-zero vector.
   TRY(csrng_testutils_kat_generate(csrng, 1, kCsrngBlockLen,
                                    kAesMaskingPrngZeroOutputSeed,
                                    &expected_state_generate));
+
+  // Check that the correct recoverable alert fired even if the alert was
+  // ignored.
+  uint32_t recov_alerts = 0;
+  TRY(dif_csrng_get_recoverable_alerts(csrng, &recov_alerts));
+  TRY_CHECK(recov_alerts == kDifCsrngRecoverableAlertRepeatedGenBits);
+  TRY(dif_csrng_clear_recoverable_alerts(csrng));
+
+  // Restore the alert if it was enabled.
+  if (was_enabled == kDifToggleEnabled) {
+    TRY(dif_alert_handler_alert_set_enabled(&alert_handler, csrng_recov_alert,
+                                            kDifToggleEnabled));
+  }
+
   return OK_STATUS();
 }
 #endif