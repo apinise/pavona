lowRISC/opentitan

[hw,ac_range_check,dv] Templify code to deal with module_instance_name
Signed-off-by: Robert Schilling <rschilling@rivosinc.com>

hw/ip_templates/ac_range_check/dv/env/ac_range_check_env.sv.tpl
@@ -2,13 +2,13 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-class ac_range_check_env extends cip_base_env #(
-    .CFG_T              (ac_range_check_env_cfg),
-    .COV_T              (ac_range_check_env_cov),
-    .VIRTUAL_SEQUENCER_T(ac_range_check_virtual_sequencer),
-    .SCOREBOARD_T       (ac_range_check_scoreboard)
+class ${module_instance_name}_env extends cip_base_env #(
+    .CFG_T              (${module_instance_name}_env_cfg),
+    .COV_T              (${module_instance_name}_env_cov),
+    .VIRTUAL_SEQUENCER_T(${module_instance_name}_virtual_sequencer),
+    .SCOREBOARD_T       (${module_instance_name}_scoreboard)
   );
-  `uvm_component_utils(ac_range_check_env)
+  `uvm_component_utils(${module_instance_name}_env)
 
   tl_agent tl_unfilt_agt;
   tl_agent tl_filt_agt;
@@ -17,14 +17,14 @@ class ac_range_check_env extends cip_base_env #(
   extern function new(string name="", uvm_component parent=null);
   extern function void build_phase(uvm_phase phase);
   extern function void connect_phase(uvm_phase phase);
-endclass : ac_range_check_env
+endclass : ${module_instance_name}_env
 
 
-function ac_range_check_env::new(string name="", uvm_component parent=null);
+function ${module_instance_name}_env::new(string name="", uvm_component parent=null);
   super.new(name, parent);
 endfunction : new
 
-function void ac_range_check_env::build_phase(uvm_phase phase);
+function void ${module_instance_name}_env::build_phase(uvm_phase phase);
   super.build_phase(phase);
 
   // Create Unfiltered TL agent
@@ -43,7 +43,7 @@ function void ac_range_check_env::build_phase(uvm_phase phase);
   end
 endfunction : build_phase
 
-function void ac_range_check_env::connect_phase(uvm_phase phase);
+function void ${module_instance_name}_env::connect_phase(uvm_phase phase);
   super.connect_phase(phase);
   if (cfg.en_scb) begin
     tl_unfilt_agt.monitor.a_chan_port.connect(

apinise/pavona

hw/ip_templates/ac_range_check/dv/env/ac_range_check_env.sv.tpl
@@ -3,13 +3,13 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-class ac_range_check_env extends cip_rst_safe_base_env #(
-    .CFG_T              (ac_range_check_env_cfg),
-    .COV_T              (ac_range_check_env_cov),
-    .VIRTUAL_SEQUENCER_T(ac_range_check_virtual_sequencer),
-    .SCOREBOARD_T       (ac_range_check_scoreboard)
+class ${module_instance_name}_env extends cip_rst_safe_base_env #(
+    .CFG_T              (${module_instance_name}_env_cfg),
+    .COV_T              (${module_instance_name}_env_cov),
+    .VIRTUAL_SEQUENCER_T(${module_instance_name}_virtual_sequencer),
+    .SCOREBOARD_T       (${module_instance_name}_scoreboard)
   );
-  `uvm_component_utils(ac_range_check_env)
+  `uvm_component_utils(${module_instance_name}_env)
 
   tl_agent tl_unfilt_agt;
   tl_agent tl_filt_agt;
@@ -21,14 +21,14 @@ class ac_range_check_env extends cip_rst_safe_base_env #(
   extern function new(string name="", uvm_component parent=null);
   extern function void build_phase(uvm_phase phase);
   extern function void connect_phase(uvm_phase phase);
-endclass : ac_range_check_env
+endclass : ${module_instance_name}_env
 
 
-function ac_range_check_env::new(string name="", uvm_component parent=null);
+function ${module_instance_name}_env::new(string name="", uvm_component parent=null);
   super.new(name, parent);
 endfunction : new
 
-function void ac_range_check_env::build_phase(uvm_phase phase);
+function void ${module_instance_name}_env::build_phase(uvm_phase phase);
   super.build_phase(phase);
 
   // Create Unfiltered TL agent
@@ -62,7 +62,7 @@ function void ac_range_check_env::build_phase(uvm_phase phase);
   end
 endfunction : build_phase
 
-function void ac_range_check_env::connect_phase(uvm_phase phase);
+function void ${module_instance_name}_env::connect_phase(uvm_phase phase);
   super.connect_phase(phase);
   if (cfg.en_scb) begin
     tl_unfilt_agt.monitor.a_chan_port.connect(